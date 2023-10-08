8294d29c839040acde00540d8f11c95a693cd6f8
Oyster Lee <oysterd3@gmail.com>
2023-10-08 15:40:05 +0800

    feat: add constants for elements

---
+++ packages/core/src/constants.ts
@@ -0,0 +1,120 @@
+export const INPUT_ELEMENTS = {
+  text: {
+    label: 'Text Input',
+    value: 'TEXT',
+  },
+  textArea: {
+    label: 'Text Area',
+    value: 'TEXT_AREA',
+  },
+  number: {
+    label: 'Number',
+    value: 'NUMBER',
+  },
+  select: {
+    label: 'Select',
+    value: 'SELECT',
+  },
+  checkbox: {
+    label: 'Checkbox',
+    value: 'CHECKBOX',
+  },
+  radio: {
+    label: 'Radio',
+    value: 'RADIO',
+  },
+  date: {
+    label: 'Date',
+    value: 'DATE',
+  },
+  time: {
+    label: 'Time',
+    value: 'TIME',
+  },
+  file: {
+    label: 'File',
+    value: 'FILE',
+  },
+  richText: {
+    label: 'Rich Text',
+    value: 'RICH_TEXT',
+  },
+  range: {
+    label: 'Range',
+    value: 'RANGE',
+  },
+  color: {
+    label: 'Color',
+    value: 'COLOR',
+  },
+  switch: {
+    label: 'Switch',
+    value: 'SWITCH',
+  },
+  password: {
+    label: 'Password',
+    value: 'PASSWORD',
+  },
+  email: {
+    label: 'Email',
+    value: 'EMAIL',
+  },
+  url: {
+    label: 'URL',
+    value: 'URL',
+  },
+  hidden: {
+    label: 'Hidden',
+    value: 'HIDDEN',
+  },
+} as const;
+
+export const STATIC_ELEMENTS = {
+  button: {
+    label: 'Button',
+    value: 'BUTTON',
+  },
+  divider: {
+    label: 'Divider',
+    value: 'DIVIDER',
+  },
+  description: {
+    label: 'Description',
+    value: 'DESCRIPTION',
+  },
+  captcha: {
+    label: 'Captcha',
+    value: 'CAPTCHA',
+  },
+  quote: {
+    label: 'Quote',
+    value: 'QUOTE',
+  },
+  header: {
+    label: 'Header',
+    value: 'HEADER',
+  },
+  image: {
+    label: 'Image',
+    value: 'IMAGE',
+  },
+  link: {
+    label: 'Link',
+    value: 'LINK',
+  },
+  staticHTML: {
+    label: 'Static HTML',
+    value: 'STATIC_HTML',
+  },
+} as const;
+
+export const LAYOUT_ELEMENTS = {
+  steps: {
+    label: 'Steps',
+    value: 'STEPS',
+  },
+  container: {
+    label: 'Container',
+    value: 'CONTAINER',
+  },
+} as const;
