f19143375fda363ff64b89cdf23d3a9f6631c897
Oyster Lee <oysterd3@gmail.com>
2023-11-13 07:18:38 +0800

    feat: add nanoid type

--- examples/react/src/types/editor.ts
+++ examples/react/src/types/editor.ts
@@ -6,6 +6,7 @@ export interface InputDragAndDropData<
 > {
   type: typeof DRAG_AND_DROP_DATA_TYPE.input;
   element: InputElements;
+  nanoId?: string | null;
   props: T;
 }
 
@@ -14,6 +15,7 @@ export interface InputGroupDragAndDropData<
 > {
   type: typeof DRAG_AND_DROP_DATA_TYPE.group;
   element: InputGroupElements;
+  nanoId?: string | null;
   props: T;
 }
 
@@ -22,6 +24,7 @@ export interface StaticDragAndDropData<
 > {
   type: typeof DRAG_AND_DROP_DATA_TYPE.static;
   element: StaticElements;
+  nanoId?: string | null;
   props: T;
 }
 
