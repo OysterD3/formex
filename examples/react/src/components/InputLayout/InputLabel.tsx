53a922729572fa3e42fea1e2c0bd8035af837612
Oyster Lee <oysterd3@gmail.com>
2023-11-12 18:45:01 +0800

    feat: accept classname prop

--- examples/react/src/components/InputLayout/InputLabel.tsx
+++ examples/react/src/components/InputLayout/InputLabel.tsx
@@ -1,15 +1,17 @@
+import classNames from 'classnames';
 import { createBEM } from '../../utils/bem.ts';
 
 interface InputLabelProps {
   htmlFor?: string;
+  className?: string;
   children: React.ReactNode;
 }
 
 const bem = createBEM('input');
 
-const InputLabel = ({ htmlFor, children }: InputLabelProps) => {
+const InputLabel = ({ htmlFor, className, children }: InputLabelProps) => {
   return (
-    <label htmlFor={htmlFor} className={bem('label')}>
+    <label htmlFor={htmlFor} className={classNames(className, bem('label'))}>
       {children}
     </label>
   );
