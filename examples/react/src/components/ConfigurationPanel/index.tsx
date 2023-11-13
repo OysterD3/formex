be4749fe489face401cb4a5bf45f20bdeab1b425
Oyster Lee <oysterd3@gmail.com>
2023-11-13 09:35:36 +0800

    feat: add header to configuration panel

--- examples/react/src/components/ConfigurationPanel/index.tsx
+++ examples/react/src/components/ConfigurationPanel/index.tsx
@@ -1,4 +1,5 @@
 import { useFormContext, useWatch } from 'react-hook-form';
+import React from 'react';
 import { INPUT_GROUPS, INPUTS } from '../../constants.ts';
 import { DragAndDropData, FormexFormValues } from '../../types';
 import { createBEM } from '../../utils/bem.ts';
@@ -6,6 +7,9 @@ import {
   isInputDragAndDropData,
   isInputGroupDragAndDropData,
 } from '../../types/guard.ts';
+import IconButton from '../StaticElements/IconButton.tsx';
+import { getElementAttribute } from '../../utils/dom.ts';
+import { useFormexFields } from '../FormexProvider.tsx';
 import ConfigurationTextField from './TextField.tsx';
 import ConfigurationTextArea from './TextArea.tsx';
 import ConfigurationTimePicker from './TimePicker.tsx';
@@ -76,35 +80,53 @@ const InputGroupConfigurationPanel = ({
   }
 };
 
-const ConfigurationPanel = () => {
+const ConfigurationPanel = ({
+  onRemove,
+}: {
+  onRemove?: (index: number) => void;
+}) => {
   const { control } = useFormContext<FormexFormValues>();
-  const [activeIndex, items] = useWatch({
-    name: ['activeIndex', 'items'],
+  const [activeIndex] = useWatch({
+    name: ['activeIndex'],
     control,
   });
+  const { remove, fields } = useFormexFields();
+
+  const handleDelete = (e: React.MouseEvent<HTMLDivElement>) => {
+    const data = getElementAttribute(e, 'data-delete-index');
+    if (data !== null) {
+      remove(parseInt(data));
+      onRemove?.(parseInt(data));
+    }
+  };
 
   return (
-    <div className={bem('container')}>
-      {items.map((item, index) => {
-        if (isInputDragAndDropData(item)) {
-          return (
-            <InputConfigurationPanel
-              key={index}
-              item={item}
-              isSelected={activeIndex === index}
-            />
-          );
-        }
-        if (isInputGroupDragAndDropData(item)) {
-          return (
-            <InputGroupConfigurationPanel
-              key={index}
-              item={item}
-              isSelected={activeIndex === index}
-            />
-          );
-        }
-        return null;
+    <div className={bem('container')} onClick={handleDelete}>
+      {fields.map((item, index) => {
+        if (activeIndex !== index) return null;
+
+        return (
+          <React.Fragment key={index}>
+            <div className={bem('heading-wrapper')}>
+              <h2 className={bem('heading')}>Configuration Panel</h2>
+              <IconButton color="danger" data-delete-index={index}>
+                <div className="i-ri-delete-bin-2-line w-full h-full" />
+              </IconButton>
+            </div>
+            {isInputDragAndDropData(item) && (
+              <InputConfigurationPanel
+                item={item}
+                isSelected={activeIndex === index}
+              />
+            )}
+            {isInputGroupDragAndDropData(item) && (
+              <InputGroupConfigurationPanel
+                item={item}
+                isSelected={activeIndex === index}
+              />
+            )}
+          </React.Fragment>
+        );
       })}
     </div>
   );
