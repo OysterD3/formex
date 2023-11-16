8bed2cacebe9baf2533f63152c2c754e7c4358ba
Oyster Lee <oysterd3@gmail.com>
2023-11-16 12:11:09 +0800

    refactor: update select config panel styles

--- examples/react/src/components/ConfigurationPanel/Select.tsx
+++ examples/react/src/components/ConfigurationPanel/Select.tsx
@@ -10,6 +10,9 @@ import { SelectProps } from '../Inputs/Select';
 import Button from '../StaticElements/Button.tsx';
 import IconButton from '../StaticElements/IconButton.tsx';
 import Switch from '../Inputs/Switch.tsx';
+import { createBEM } from '../../utils/bem.ts';
+
+const bem = createBEM('configuration-panel-select');
 
 const ConfigurationSelect = () => {
   const { control } =
@@ -71,32 +74,45 @@ const ConfigurationSelect = () => {
         name={`items.${index}.props.defaultValue`}
         control={control}
       />
-      <div className="flex flex-col gap-y-2">
+      <fieldset className={bem('option-container')}>
+        <legend className="formex-input__label">Options</legend>
         {fields.map((field, i) => (
-          <div className="flex gap-x-1.5 items-center" key={field.id}>
+          <div key={field.id} className={bem('option-wrapper')}>
             <Controller
               render={({ field: { value, ...field } }) => (
-                <TextField {...field} value={value || ''} placeholder="Label" />
+                <TextField
+                  {...field}
+                  value={value || ''}
+                  label=""
+                  placeholder="Label"
+                />
               )}
               name={`items.${index}.props.options.${i}.label`}
               control={control}
             />
             <Controller
               render={({ field: { value, ...field } }) => (
-                <TextField {...field} value={value || ''} placeholder="Value" />
+                <TextField
+                  {...field}
+                  value={value || ''}
+                  label=""
+                  placeholder="Value"
+                />
               )}
               name={`items.${index}.props.options.${i}.value`}
               control={control}
             />
-            <IconButton onClick={() => remove(i)}>
-              <div className="i-ri-delete-bin-2-line w-full h-full"></div>
-            </IconButton>
+            {fields.length > 1 && (
+              <IconButton color="danger" onClick={() => remove(i)}>
+                <div className="i-ri-delete-bin-2-line w-full h-full"></div>
+              </IconButton>
+            )}
           </div>
         ))}
         <Button onClick={() => append({ label: '', value: '' })}>
           Add Option
         </Button>
-      </div>
+      </fieldset>
       <Controller
         render={({ field }) => <Switch {...field} label="Disabled" />}
         name={`items.${index}.props.disabled`}
