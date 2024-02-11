137604a5fca0b342bf0f6af1779b2662d141667f
Oyster Lee <oysterd3@gmail.com>
2024-02-12 07:48:35 +0800

    style: reformat

--- packages/core/src/types/props.ts
+++ packages/core/src/types/props.ts
@@ -167,74 +167,74 @@ export type InputElementProps<T extends InputElements | AllValue> =
   T extends typeof INPUTS.singleLineText
     ? SingleLineTextProps
     : T extends typeof INPUTS.multiLineText
-    ? MultiLineTextProps
-    : T extends typeof INPUTS.file
-    ? FileUploadProps
-    : T extends typeof INPUTS.checkbox
-    ? CheckboxProps
-    : T extends typeof INPUTS.radio
-    ? RadioProps
-    : T extends typeof INPUTS.select
-    ? SelectProps<unknown>
-    : T extends typeof INPUTS.number
-    ? NumberProps
-    : T extends typeof INPUTS.date
-    ? DatePickerProps
-    : T extends typeof INPUTS.time
-    ? TimePickerProps
-    : T extends typeof INPUTS.richText
-    ? RichTextProps
-    : T extends typeof INPUTS.switch
-    ? SwitchProps
-    : T extends typeof INPUTS.email
-    ? EmailProps
-    : T extends typeof INPUTS.password
-    ? PasswordProps
-    : T extends typeof INPUTS.phone
-    ? PhoneProps
-    : T extends typeof INPUTS.url
-    ? URLProps
-    : T extends typeof INPUTS.datetime
-    ? DateTimePickerProps
-    : T extends typeof INPUTS.address
-    ? AddressProps
-    : T extends AllValue
-    ? SingleLineTextProps &
-        MultiLineTextProps &
-        FileUploadProps &
-        CheckboxProps &
-        RadioProps &
-        SelectProps &
-        NumberProps &
-        DatePickerProps &
-        TimePickerProps &
-        RichTextProps &
-        SwitchProps &
-        EmailProps &
-        PasswordProps &
-        PhoneProps &
-        URLProps &
-        DateTimePickerProps &
-        AddressProps
-    : never;
+      ? MultiLineTextProps
+      : T extends typeof INPUTS.file
+        ? FileUploadProps
+        : T extends typeof INPUTS.checkbox
+          ? CheckboxProps
+          : T extends typeof INPUTS.radio
+            ? RadioProps
+            : T extends typeof INPUTS.select
+              ? SelectProps<unknown>
+              : T extends typeof INPUTS.number
+                ? NumberProps
+                : T extends typeof INPUTS.date
+                  ? DatePickerProps
+                  : T extends typeof INPUTS.time
+                    ? TimePickerProps
+                    : T extends typeof INPUTS.richText
+                      ? RichTextProps
+                      : T extends typeof INPUTS.switch
+                        ? SwitchProps
+                        : T extends typeof INPUTS.email
+                          ? EmailProps
+                          : T extends typeof INPUTS.password
+                            ? PasswordProps
+                            : T extends typeof INPUTS.phone
+                              ? PhoneProps
+                              : T extends typeof INPUTS.url
+                                ? URLProps
+                                : T extends typeof INPUTS.datetime
+                                  ? DateTimePickerProps
+                                  : T extends typeof INPUTS.address
+                                    ? AddressProps
+                                    : T extends AllValue
+                                      ? SingleLineTextProps &
+                                          MultiLineTextProps &
+                                          FileUploadProps &
+                                          CheckboxProps &
+                                          RadioProps &
+                                          SelectProps &
+                                          NumberProps &
+                                          DatePickerProps &
+                                          TimePickerProps &
+                                          RichTextProps &
+                                          SwitchProps &
+                                          EmailProps &
+                                          PasswordProps &
+                                          PhoneProps &
+                                          URLProps &
+                                          DateTimePickerProps &
+                                          AddressProps
+                                      : never;
 
 export type InputGroupElementProps<T extends InputGroupElements | AllValue> =
   T extends typeof INPUT_GROUPS.radio
     ? RadioGroupProps
     : T extends typeof INPUT_GROUPS.checkbox
-    ? CheckboxGroupProps
-    : T extends AllValue
-    ? RadioGroupProps & CheckboxGroupProps
-    : never;
+      ? CheckboxGroupProps
+      : T extends AllValue
+        ? RadioGroupProps & CheckboxGroupProps
+        : never;
 
 export type ElementProps<T extends Elements | AllValue> =
   T extends InputElements
     ? InputElementProps<T>
     : T extends InputGroupElements
-    ? InputGroupElementProps<T>
-    : T extends AllValue
-    ? InputElementProps<AllValue> & InputGroupElementProps<AllValue>
-    : never;
+      ? InputGroupElementProps<T>
+      : T extends AllValue
+        ? InputElementProps<AllValue> & InputGroupElementProps<AllValue>
+        : never;
 
 export type EditorComponentProps<T extends object> = Omit<
   T,
